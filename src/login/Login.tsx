17599346a35d2c702a167d46cd9195e100697e0c
heutronica <heutronica@gmail.com>
2022-05-20 21:17:03 +0900

    feat: Routing configuration for /login and /signup

---
+++ src/login/Login.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { RouteProps } from "react-router-dom";
+
+export const Login: React.FC<RouteProps> = () => {
+  return (
+    <>
+      <div>Login</div>
+    </>
+  );
+};
---
+++ src/signup/Signup.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { RouteProps } from "react-router-dom";
+
+export const Signup: React.FC<RouteProps> = () => {
+  return (
+    <>
+      <div>Signup</div>
+    </>
+  );
+};
