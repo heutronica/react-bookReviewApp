01849922e5b3cebf2ed2f814f9dce894ec7c7cab
heutronica <heutronica@gmail.com>
2022-05-27 21:25:20 +0900

    style: UIコンポーネントライブラリとしてMantineUIを追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+
+import { MantineProvider, MantineThemeOverride } from '@mantine/core'
+
+const myTheme: MantineThemeOverride = {
+    colorScheme: 'light',
+    primaryColor: 'orange',
+    defaultRadius: 0,
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <MantineProvider theme={myTheme} withGlobalStyles withNormalizeCSS>
+            <App />
+        </MantineProvider>
+    </React.StrictMode>
 )
