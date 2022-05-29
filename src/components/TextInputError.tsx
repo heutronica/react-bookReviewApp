e69895d3c1d612c3a3eeaa6c650a1a36d54024b7
heutronica <heutronica@gmail.com>
2022-05-29 22:39:22 +0900

    refactor: propsの部分

--- src/components/TextInputError.tsx
+++ src/components/TextInputError.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Alert } from '@mantine/core'
 
-export const TextInputError = (props) => {
-    return <Alert title="Error">{props.children}</Alert>
+export const TextInputError = ({ children }: { children: React.ReactNode }) => {
+    return <Alert title="Error">{children}</Alert>
 }
