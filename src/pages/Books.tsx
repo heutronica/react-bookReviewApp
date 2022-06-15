fbe3820e42e2d4e95f90bb266d6b89ef584c75bd
heutronica <heutronica@gmail.com>
2022-06-15 14:13:00 +0900

    feat: 前の10件・次の10件の読み込み

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from 'react'
+import {
+    useSearchParams,
+    useParams,
+    createSearchParams,
+} from 'react-router-dom'
+
 import { LinkButton } from '../components/LinkButton'
+import { Button } from '../components/Button'
 import { BookCard } from '../components/BookCard'
-import { css } from '@emotion/react'
-import { theme } from '../style/theme'
 import { Header } from '../components/Header'
 import { Footer } from '../components/Footer'
+
+import { css } from '@emotion/react'
+import { theme } from '../style/theme'
 
 interface Book {
     detail: string
@@ -17,20 +25,38 @@ interface Book {
 
 export const Books = () => {
     const [books, setBooks] = useState<Book[]>([])
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const handleNextPageChange = () => {
+        const offset = Number(searchParams.get('offset'))
+        if (!offset) return setSearchParams({ offset: '10' })
+        setSearchParams({ offset: (offset + 10).toString() })
+    }
+
+    const handlePrevPageChange = () => {
+        const offset = Number(searchParams.get('offset'))
+        if (!offset || offset <= 10) return setSearchParams({})
+        setSearchParams({ offset: (offset - 10).toString() })
+    }
 
     useEffect(() => {
-        fetch(
-            'https://api-for-missions-and-railways.herokuapp.com/public/books',
-            {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            }
-        )
+        const getOffset = searchParams.get('offset')
+        let bookAPIURL: string
+        // 美しくないなあ
+        if (!getOffset) {
+            bookAPIURL = `https://api-for-missions-and-railways.herokuapp.com/public/books`
+        } else {
+            bookAPIURL = `https://api-for-missions-and-railways.herokuapp.com/public/books?offset=${getOffset}`
+        }
+        fetch(bookAPIURL, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
             .then((response) => response.json())
             .then((response) => setBooks(response))
-    }, [''])
+    }, ['', searchParams])
 
     return (
         <>
@@ -48,6 +74,18 @@ export const Books = () => {
                         {books.map((props) => (
                             <BookCard {...props} key={props.id} />
                         ))}
+                    </div>
+                    <div css={styles.pageNav.wrapper}>
+                        <div css={styles.pageNav.prev}>
+                            {searchParams.get('offset') && (
+                                <Button onClick={handlePrevPageChange}>
+                                    PREV
+                                </Button>
+                            )}
+                        </div>
+                        <div css={styles.pageNav.next}>
+                            <Button onClick={handleNextPageChange}>NEXT</Button>
+                        </div>
                     </div>
                 </div>
             </main>
@@ -78,7 +116,28 @@ const styles = {
     bookList: css({
         display: 'grid',
         gridTemplateColumns: 'repeat(2, 1fr)',
-        gap: '20px',
+        gap: '1rem',
         [mq[1]]: { gridTemplateColumns: '1fr' },
     }),
+    pageNav: {
+        wrapper: css({
+            display: 'flex',
+            flexBasis: 'auto',
+            justifyContent: 'space-between',
+            columnGap: '1rem',
+            padding: '2rem 0',
+
+            [mq[0]]: {
+                padding: '',
+            },
+        }),
+        prev: css({
+            width: '100%',
+            textAlign: 'right',
+        }),
+        next: css({
+            width: '100%',
+            textAlign: 'left',
+        }),
+    },
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import { Header } from '../components/Header'
 import { Footer } from '../components/Footer'
 import { BookCard } from '../components/BookCard'
+import { Button } from '../components/Button'
 
 import { css } from '@emotion/react'
 import { theme } from '../style/theme'
 
 import { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 interface Book {
     detail: string
@@ -18,10 +20,31 @@ interface Book {
 
 export const Home = () => {
     const [books, setBooks] = useState<Book[]>([])
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const handleNextPageChange = () => {
+        const offset = Number(searchParams.get('offset'))
+        if (!offset) return setSearchParams({ offset: '10' })
+        setSearchParams({ offset: (offset + 10).toString() })
+    }
+
+    const handlePrevPageChange = () => {
+        const offset = Number(searchParams.get('offset'))
+        if (!offset || offset <= 10) return setSearchParams({})
+        setSearchParams({ offset: (offset - 10).toString() })
+    }
 
     useEffect(() => {
         const token = sessionStorage.getItem('auth.token')
-        fetch('https://api-for-missions-and-railways.herokuapp.com/books', {
+        const getOffset = searchParams.get('offset')
+        let bookAPIURL: string
+        // 美しくないなあ
+        if (!getOffset) {
+            bookAPIURL = `https://api-for-missions-and-railways.herokuapp.com/books`
+        } else {
+            bookAPIURL = `https://api-for-missions-and-railways.herokuapp.com/books?offset=${getOffset}`
+        }
+        fetch(bookAPIURL, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -30,7 +53,7 @@ export const Home = () => {
         })
             .then((response) => response.json())
             .then((response) => setBooks(response))
-    }, [''])
+    }, ['', searchParams])
 
     return (
         <>
@@ -40,6 +63,16 @@ export const Home = () => {
                     {books.map((props) => (
                         <BookCard {...props} key={props.id} />
                     ))}
+                </div>
+                <div css={styles.pageNav.wrapper}>
+                    <div css={styles.pageNav.prev}>
+                        {searchParams.get('offset') && (
+                            <Button onClick={handlePrevPageChange}>PREV</Button>
+                        )}
+                    </div>
+                    <div css={styles.pageNav.next}>
+                        <Button onClick={handleNextPageChange}>NEXT</Button>
+                    </div>
                 </div>
             </main>
             <Footer />
@@ -52,13 +85,33 @@ const mq = breakpoints.map((bp) => `@media (max-width: ${bp})`)
 
 const styles = {
     main: css({ padding: '0 1.2rem' }),
-
     bookList: css({
         margin: '0 auto',
         maxWidth: theme.breakpoints.lg,
         display: 'grid',
         gridTemplateColumns: 'repeat(2, 1fr)',
-        gap: '20px',
+        gap: '1rem',
         [mq[1]]: { gridTemplateColumns: '1fr' },
     }),
+    pageNav: {
+        wrapper: css({
+            display: 'flex',
+            flexBasis: 'auto',
+            justifyContent: 'space-between',
+            columnGap: '1rem',
+            padding: '2rem 0',
+
+            [mq[0]]: {
+                padding: '',
+            },
+        }),
+        prev: css({
+            width: '100%',
+            textAlign: 'right',
+        }),
+        next: css({
+            width: '100%',
+            textAlign: 'left',
+        }),
+    },
 }
